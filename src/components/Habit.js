0909bdeaca4fa7c33025e070f2d872739f7af192
pwintkooo <23029589@myrp.edu.sg>
2024-12-02 14:36:43 +0800

    L7

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -6,4 +6,16 @@ import React from "react";
  *
  * TODO: implement the Habit component here
  */
-export default function Habit() {}
+export default function Habit({ habit, onToggleHabit, onDeleteHabit }) {
+  return (
+    <li style={{ textDecoration: habit.completed ? "line-through" : "none" }}>
+      <input
+        type="checkbox"
+        checked={habit.completed}
+        onChange={() => onToggleHabit(habit.id)}
+      />
+      <span>{habit.name}</span>
+      <button onClick={() => onDeleteHabit(habit.id)}>Delete</button>
+    </li>
+  );
+}
